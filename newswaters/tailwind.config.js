e5f5c3b433eae39a93f1ddb2b4bbaec96e133667
Metal Whale <metalwhale.dev@gmail.com>
2023-11-02 13:28:44 +0000

    Add "tailwind".

---
+++ newswaters/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // See: https://tailwindcss.com/docs/guides/vite#vue
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
